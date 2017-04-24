65a982f28a483eacab0b996fa2d393fab8f32caa
Nicolás Berríos Verboven <nbverboven@gmail.com>
2017-04-24 10:47:49 -0300

    Empecé con C_linearZoom.c

--- src/filters/C_linearZoom.c
+++ src/filters/C_linearZoom.c
@@ -9,6 +9,8 @@
 #include <math.h>
 
 void C_linearZoom(uint8_t* src, uint32_t srcw, uint32_t srch,
-                  uint8_t* dst, uint32_t dstw, uint32_t dsth __attribute__((unused))) {
+                  uint8_t* dst, uint32_t dstw, uint32_t dsth __attribute__((unused)))
+{
+	// Nico está haciendo esto
 }
 
