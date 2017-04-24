84ef0213d362e99fff6a2a0eee555cebab9528ea
Federico <federico2102@gmail.com>
2017-04-23 21:05:47 -0300

    Funcion para convertir de rgb a yuv implementada y funcionando

--- src/filters/C_convert.c
+++ src/filters/C_convert.c
@@ -8,9 +8,35 @@
 #include "filters.h"
 #include <math.h>
 
-
 void C_convertRGBtoYUV(uint8_t* src, uint32_t srcw, uint32_t srch,
                        uint8_t* dst, uint32_t dstw, uint32_t dsth __attribute__((unused))) {
+
+	uint8_t *A, B, G, R, *V, *U, *Y; 
+	uint32_t i = 0;
+	while(i < 4*srch){ // No se por que si no multiplico por 4 hace solo 1/4 de la imagen
+
+		uint32_t j = 0;
+		while(j < srcw){
+
+			B = *(src+srcw*i+j+1);
+			G = *(src+srcw*i+j+2);
+			R = *(src+srcw*i+j+3);
+
+			A = dst+srcw*i+j;
+			V = dst+srcw*i+j+1;
+			U = dst+srcw*i+j+2;
+			Y = dst+srcw*i+j+3;
+
+			*A = *(src+srcw*i+j); // La componente A se mantiene igual
+			*Y = fmin((((66 * R + 129 * G + 25 * B + 128) >> 8) + 16),255);
+			*U = fmax(fmin((((-38 * R - 74 * G + 112 * B + 128) >> 8) + 128),255),0);
+			*V = fmax(fmin((((112 * R - 94 * G - 18 * B + 128) >> 8) + 128),255),0);
+
+			j += 4;
+		}
+
+		i++;	
+	}
 }
 
 void C_convertYUVtoRGB(uint8_t* src, uint32_t srcw, uint32_t srch,
