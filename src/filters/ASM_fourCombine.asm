e7beb92866f24a6111667ed8c71b149bc9d02065
Federico2102 <federico2102@gmail.com>
2017-05-05 01:15:43 -0300

    Despues de mucho quilombo compilo. Ahora compila y corre pero me genera una imagen en negro.

--- src/filters/ASM_fourCombine.asm
+++ src/filters/ASM_fourCombine.asm
@@ -24,17 +24,17 @@ ASM_fourCombine:
         push r15
         ; Pila alineada
 
-        mov r12, rdi    ;r12= puntero src
-        mov r13, rcx    ;r13= puntero dst
+        mov r12, rdi    	;r12= puntero src
+        mov r13, rcx    	;r13= puntero dst
         mov r14, rsi	;r14= srcw
 
         xor rdx, rdx
         xor rdi, rdi
-        mov eax, rsi  	;eax= srcw
-        mov edi, rdx  	;edi= srch
+        mov eax, esi  	;eax= srcw
+        mov edi, edx  	;edi= srch
         mul edi         ;rax= srcw*srch
         mov rdi, rax	;rdi= srcw*srch
-        add rdi, rdi
+        add rdi, rdi	;rdi= srcw*srch*2. Es la mitad de la cantidad total de bytes de la imagen
         mov ebx, offset_ARGB
         mul ebx         ;rax= srcw*srch*4
         mov r15, rax    ;r15= srcw*srch*4. Es la cantidad de bytes de la imagen
@@ -55,17 +55,32 @@ ASM_fourCombine:
 		shufps xmm0, xmm2, 0xDD			; xmm0 = [pixel[1][1] | pixel[1][3] | pixel[1][5] | pixel[1][7]]
 		shufps xmm1, xmm2, 0x88			; xmm1 = [pixel[1][2] | pixel[1][4] | pixel[1][6] | pixel[1][8]]
 
-		movdqu xmm4, [r12+rbx+4*r8] 	; xmm4 = [pixel[2][1] | pixel[2][2] | pixel[2][3] | pixel[2][4]]
-		movdqu xmm3, [r12+rbx+4*r8+16] 	; xmm3 = [pixel[2][5] | pixel[2][6] | pixel[2][7] | pixel[2][8]]
+		xor rcx, rcx
+		mov rcx, r8
+		add rcx, rcx
+		add rcx, rcx
+		add rcx, rbx
+		movdqu xmm4, [r12+rcx] 			; xmm4 = [pixel[2][1] | pixel[2][2] | pixel[2][3] | pixel[2][4]]
+		movdqu xmm3, [r12+rcx] 			; xmm3 = [pixel[2][5] | pixel[2][6] | pixel[2][7] | pixel[2][8]]
 		movdqu xmm2, xmm3				; xmm2 = [pixel[2][5] | pixel[2][6] | pixel[2][7] | pixel[2][8]]
 
 		shufps xmm2, xmm4, 0xDD			; xmm2 = [pixel[2][1] | pixel[2][3] | pixel[2][5] | pixel[2][7]]
 		shufps xmm3, xmm4, 0x88			; xmm3 = [pixel[2][2] | pixel[2][4] | pixel[2][6] | pixel[2][8]]
 
+		xor rcx, rcx
+		mov rcx, r9
+		add rcx, rdi
 		movdqu [r13+r9], xmm0
-		movdqu [r13+r9+(2*r8)], xmm1
-		movdqu [r13+r9+rdi], xmm2
-		movdqu [r13+r9+rdi+(2*r8)], xmm3
+		movdqu [r13+rcx], xmm2
+
+		xor rcx, rcx
+		mov rcx, r8
+		add rcx, rcx
+		add rcx, r9
+		movdqu [r13+rcx], xmm1
+
+		add rcx, rdi
+		movdqu [r13+rcx], xmm3
 
 		add rbx, 32
 		add r9, 16
