f24b2246aa316c25359fb333f0970a5becffc259
Nicolás Berríos Verboven <nbverboven@gmail.com>
2017-05-09 10:13:40 -0300

    Ahí está arreglado el desplazamiento de la imagen. Falta agregar los bordes

--- src/filters/ASM_linearZoom.asm
+++ src/filters/ASM_linearZoom.asm
@@ -55,63 +55,63 @@ ASM_linearZoom:
 
 	.genero_filas_modificadas:
 
-		movdqu     xmm0, [rbx]         ; xmm0 <- [ src[n-1][0] | src[n-1][1] | src[n-1][2] | src[n-1][3] ]
-		movdqu     xmm1, [rbx + 4*rsi] ; xmm1 <- [ src[n-2][0] | src[n-2][1] | src[n-2][2] | src[n-2][3] ]
+		; Versión 2.1
 
-		; Versión 2.0
-
-		psrldq    xmm0, 4 ; xmm0 <- [      0      | src[n-1][0] | src[n-1][1] | src[n-1][2] ]
-		psrldq    xmm1, 4 ; xmm1 <- [      0      | src[n-2][0] | src[n-2][1] | src[n-2][2] ]
+		movdqu     xmm0, [rbx]         ; xmm0 <- [ src[n-1][3] | src[n-1][2] | src[n-1][1] | src[n-1][0] ]
+		movdqu     xmm1, [rbx + 4*rsi] ; xmm1 <- [ src[n-2][3] | src[n-2][2] | src[n-2][1] | src[n-2][0] ]
 
 		movdqu    xmm2, xmm0
 		movdqu    xmm3, xmm1
 
-		psrldq    xmm2, 4 ; xmm2 <- [      0      |      0      | src[n-1][0] | src[n-1][1] ]
-		psrldq    xmm3, 4 ; xmm3 <- [      0      |      0      | src[n-2][0] | src[n-2][1] ]
+		pslldq    xmm0, 8 ; xmm0 <- [ src[n-1][1] | src[n-1][0] |      0      |      0      ]
+		pslldq    xmm1, 8 ; xmm1 <- [ src[n-2][1] | src[n-2][0] |      0      |      0      ]
 
-		punpcklbw  xmm0, xmm8 
-		punpcklbw  xmm1, xmm8
+		psrldq    xmm2, 4 ; xmm2 <- [      0      | src[n-1][3] | src[n-1][2] | src[n-1][1] ]
+		psrldq    xmm3, 4 ; xmm3 <- [      0      | src[n-2][3] | src[n-2][2] | src[n-2][1] ]
+
+		punpckhbw  xmm0, xmm8 
+		punpckhbw  xmm1, xmm8
 		punpcklbw  xmm2, xmm8
 		punpcklbw  xmm3, xmm8
 		
-		; xmm0 <- [ src[n-1][1] | src[n-1][2] ]
-		; xmm1 <- [ src[n-2][1] | src[n-2][2] ]
-		; xmm2 <- [ src[n-1][0] | src[n-1][1] ]
-		; xmm3 <- [ src[n-2][0] | src[n-2][1] ]		
+		; xmm0 <- [ src[n-1][1] | src[n-1][0] ]
+		; xmm1 <- [ src[n-2][1] | src[n-2][0] ]
+		; xmm2 <- [ src[n-1][2] | src[n-1][1] ]
+		; xmm3 <- [ src[n-2][2] | src[n-2][1] ]		
 
 		movdqu      xmm4, xmm1
-		paddw       xmm4, xmm0 ; xmm4 <- [ src[n-2][1] + src[n-1][1] | src[n-2][2] + src[n-1][2] ]
+		paddw       xmm4, xmm0 ; xmm4 <- [ src[n-2][1] + src[n-1][1] | src[n-2][0] + src[n-1][0] ]
 		movdqu      xmm6, xmm4
-		psrlw       xmm4, 1    ; xmm4 <- [ (src[n-2][1] + src[n-1][1]) / 2 | (src[n-2][2] + src[n-1][2]) / 2 ]
+		psrlw       xmm4, 1    ; xmm4 <- [ (src[n-2][1] + src[n-1][1]) / 2 | (src[n-2][0] + src[n-1][0]) / 2 ]
 
 		movdqu      xmm5, xmm3
-		paddw       xmm5, xmm2 ; xmm5 <- [ src[n-2][0] + src[n-1][0] | src[n-2][1] + src[n-1][1] ]
-		paddw       xmm6, xmm5 ; xmm6 <- [ src[n-2][0] + src[n-1][0] + src[n-2][1] + src[n-1][1] | src[n-2][1] + src[n-1][1] + src[n-2][2] + src[n-1][2] ]
-		psrlw       xmm5, 1    ; xmm5 <- [ (src[n-2][0] + src[n-1][0]) / 2 | (src[n-2][1] + src[n-1][1]) / 2 ]
-		psrlw       xmm6, 2    ; xmm6 <- [ (src[n-2][0] + src[n-1][0] + src[n-2][1] + src[n-1][1]) / 4 | (src[n-2][1] + src[n-1][1] + src[n-2][2] + src[n-1][2]) / 4 ]
+		paddw       xmm5, xmm2 ; xmm5 <- [ src[n-2][2] + src[n-1][2] | src[n-2][1] + src[n-1][1] ]
+		paddw       xmm6, xmm5 ; xmm6 <- [ src[n-2][1] + src[n-1][1] + src[n-2][2] + src[n-1][2] | src[n-2][0] + src[n-1][0] + src[n-2][1] + src[n-1][1] ]
+		psrlw       xmm5, 1    ; xmm5 <- [ (src[n-2][2] + src[n-1][2]) / 2 | (src[n-2][1] + src[n-1][1]) / 2 ]
+		psrlw       xmm6, 2    ; xmm6 <- [ (src[n-2][1] + src[n-1][1] + src[n-2][2] + src[n-1][2]) / 4 | (src[n-2][0] + src[n-1][0] + src[n-2][1] + src[n-1][1]) / 4 ]
 
-		paddw       xmm1, xmm3
-		psrlw       xmm1, 1    ; xmm1 <- [ (src[n-2][1] + src[n-2][0]) / 2 | (src[n-2][2] + src[n-2][1]) / 2 ]
+		paddw       xmm3, xmm1
+		psrlw       xmm3, 1    ; xmm3 <- [ (src[n-2][2] + src[n-2][1]) / 2 | (src[n-2][1] + src[n-2][0]) / 2 ]
 
-		packuswb    xmm1, xmm8 ; xmm1 <- [      0      |      0      | (src[n-2][1] + src[n-2][0]) / 2 | (src[n-2][2] + src[n-2][1]) / 2 ]
-		packuswb    xmm3, xmm8 ; xmm3 <- [      0      |      0      |           src[n-2][0]           |           src[n-2][1]           ]
-		packuswb    xmm5, xmm8 ; xmm5 <- [      0      |      0      | (src[n-2][0] + src[n-1][0]) / 2 | (src[n-2][1] + src[n-1][1]) / 2 ]
-		packuswb    xmm6, xmm8 ; xmm6 <- [      0      |      0      | (src[n-2][0] + src[n-1][0] + src[n-2][1] + src[n-1][1]) / 4 | (src[n-2][1] + src[n-1][1] + src[n-2][2] + src[n-1][2]) / 4 ]
+		packuswb    xmm1, xmm8 ; xmm1 <- [      0      |      0      |           src[n-2][1]           |           src[n-2][0]           ]
+		packuswb    xmm3, xmm8 ; xmm3 <- [      0      |      0      | (src[n-2][2] + src[n-2][1]) / 2 | (src[n-2][1] + src[n-2][0]) / 2 ]
+		packuswb    xmm4, xmm8 ; xmm5 <- [      0      |      0      | (src[n-2][1] + src[n-1][1]) / 2 | (src[n-2][0] + src[n-1][0]) / 2 ]
+		packuswb    xmm6, xmm8 ; xmm6 <- [      0      |      0      | (src[n-2][1] + src[n-1][1] + src[n-2][2] + src[n-1][2]) / 4 | (src[n-2][0] + src[n-1][0] + src[n-2][1] + src[n-1][1]) / 4 ]
 
 		pslldq     xmm3, 8
-		por        xmm1, xmm3       ; xmm1 <- [ src[n-2][0] |           src[n-2][1]           | (src[n-2][1] + src[n-2][0]) / 2 | (src[n-2][2] + src[n-2][1]) / 2 ]
-		pshufd     xmm1, xmm1, 0xD8 ; xmm1 <- [ src[n-2][0] | (src[n-2][1] + src[n-2][0]) / 2 |           src[n-2][1]           | (src[n-2][2] + src[n-2][1]) / 2 ]
+		por        xmm1, xmm3       ; xmm1 <- [ (src[n-2][2] + src[n-2][1]) / 2 | (src[n-2][1] + src[n-2][0]) / 2 |           src[n-2][1]           | src[n-2][0] ]
+		pshufd     xmm1, xmm1, 0xD8 ; xmm1 <- [ (src[n-2][2] + src[n-2][1]) / 2 |           src[n-2][1]           | (src[n-2][1] + src[n-2][0]) / 2 | src[n-2][0] ]
 
-		pslldq     xmm5, 8
-		por        xmm6, xmm5
-		pshufd     xmm6, xmm6, 0xD8
+		pslldq     xmm6, 8
+		por        xmm4, xmm6
+		pshufd     xmm4, xmm4, 0xD8
 
 
 	; --------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
-	movdqu    [r13], xmm6
+	movdqu    [r13], xmm4
 	movdqu    [r13 + 4*r8], xmm1
 
 	; mov       r10, r13
@@ -231,170 +231,3 @@ ASM_linearZoom:
 	pop     rbx
 	pop     rbp
 	ret
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-; ; --------------------------------------------------------------------------------------------------------------------------------------------------
-
-; 		movdqu       xmm4, xmm0 ; xmm4 == xmm0
-; 		movdqu       xmm5, xmm0 ; xmm5 == xmm0
-
-; 		psrldq       xmm4, 4    ; xmm4 <- [ 0 | src[n-1][0] | src[n-1][1] | src[n-1][2] ]
-; 		psrldq       xmm5, 8    ; xmm5 <- [ 0 |      0      | src[n-1][0] | src[n-1][1] ]
-
-; 		punpcklbw    xmm4, xmm8 ; xmm4 <- [ 0 | src[n-1][1].a | 0 | src[n-1][1].r | 0 | src[n-1][1].g | 0 | src[n-1][1].b | 0 | src[n-1][2].a | 0 | src[n-1][2].r | 0 | src[n-1][2].g | 0 | src[n-1][2].b ]
-; 		punpcklbw    xmm5, xmm8 ; xmm5 <- [ 0 | src[n-1][0].a | 0 | src[n-1][0].r | 0 | src[n-1][0].g | 0 | src[n-1][0].b | 0 | src[n-1][1].a | 0 | src[n-1][1].r | 0 | src[n-1][1].g | 0 | src[n-1][1].b ]
-		
-; 		paddw        xmm4, xmm5 ; xmm4 <- [  src[n-1][0] + src[n-1][1]    |  src[n-1][1] + src[n-1][2]    ]
-; 		movdqu       xmm5, xmm4 ; xmm5 == xmm4
-; 		psrlw        xmm4, 1    ; xmm4 <- [ (src[n-1][0] + src[n-1][1])/2 | (src[n-1][1] + src[n-1][2])/2 ]
-
-; 		packuswb     xmm4, xmm8 ; xmm4 <- [ 0 | 0 | (src[n-1][0] + src[n-1][1])/2 | (src[n-1][1] + src[n-1][2])/2 ]
-
-; 		; --------------------------------------------------------------------------------------------------------------------------------------------------
-
-; 		movdqu       xmm6, xmm1 ; xmm6 == xmm1
-; 		movdqu       xmm7, xmm1 ; xmm7 == xmm1
-
-; 		psrldq       xmm6, 4    ; xmm6 <- [ 0 | src[n-2][0] | src[n-2][1] | src[n-2][2] ]
-; 		psrldq       xmm7, 8    ; xmm7 <- [ 0 |      0      | src[n-2][0] | src[n-2][1] ]
-
-; 		punpcklbw    xmm6, xmm8 ; xmm6 <- [ 0 | src[n-2][1].a | 0 | src[n-2][1].r | 0 | src[n-2][1].g | 0 | src[n-2][1].b | 0 | src[n-2][2].a | 0 | src[n-2][2].r | 0 | src[n-2][2].g | 0 | src[n-2][2].b ]
-; 		punpcklbw    xmm7, xmm8 ; xmm7 <- [ 0 | src[n-2][0].a | 0 | src[n-2][0].r | 0 | src[n-2][0].g | 0 | src[n-2][0].b | 0 | src[n-2][1].a | 0 | src[n-2][1].r | 0 | src[n-2][1].g | 0 | src[n-2][1].b ]
-		
-; 		paddw        xmm6, xmm7 ; xmm6 <- [  src[n-2][0] + src[n-2][1]    |  src[n-2][1] + src[n-2][2]    ]
-; 		movdqu       xmm7, xmm6 ; xmm7 == xmm6
-; 		psrlw        xmm6, 1    ; xmm6 <- [ (src[n-2][0] + src[n-2][1])/2 | (src[n-2][1] + src[n-2][2])/2 ]
-
-; 		packuswb     xmm6, xmm8 ; xmm6 <- [ 0 | 0 | (src[n-2][0] + src[n-2][1])/2 | (src[n-2][1] + src[n-2][2])/2 ]
-
-; 		; --------------------------------------------------------------------------------------------------------------------------------------------------
-
-; 		movdqu       xmm2, xmm0 ; xmm2 == xmm0
-; 		movdqu       xmm3, xmm1 ; xmm3 == xmm1
-
-; 		psrldq       xmm2, 8    ; xmm2 <- [ 0 | 0 | src[n-1][0] | src[n-1][1] ]
-; 		psrldq       xmm3, 8    ; xmm3 <- [ 0 | 0 | src[n-2][0] | src[n-2][1] ]
-
-; 		punpcklbw    xmm2, xmm8 ; xmm2 <- [ 0 | src[n-1][0].a | 0 | src[n-1][0].r | 0 | src[n-1][0].g | 0 | src[n-1][0].b | 0 | src[n-1][1].a | 0 | src[n-1][1].r | 0 | src[n-1][1].g | 0 | src[n-1][1].b ]
-; 		punpcklbw    xmm3, xmm8 ; xmm3 <- [ 0 | src[n-2][0].a | 0 | src[n-2][0].r | 0 | src[n-2][0].g | 0 | src[n-2][0].b | 0 | src[n-2][1].a | 0 | src[n-2][1].r | 0 | src[n-2][1].g | 0 | src[n-2][1].b ]
-
-; 		paddw        xmm2, xmm3 ; xmm2 <- [  src[n-1][0] + src[n-2][0]    |  src[n-1][1] + src[n-2][1]    ]
-; 		psrlw        xmm2, 1    ; xmm2 <- [ (src[n-1][0] + src[n-2][0])/2 | (src[n-1][1] + src[n-2][1])/2 ]
-
-; 		packuswb     xmm2, xmm8 ; xmm2 <- [ 0 | 0 | (src[n-1][0] + src[n-2][0])/2 | (src[n-1][1] + src[n-2][1])/2 ]
-
-; 		; --------------------------------------------------------------------------------------------------------------------------------------------------
-
-; 		; xmm5 <- [ src[n-1][0] + src[n-1][1] | src[n-1][1] + src[n-1][2] ]
-; 		; xmm7 <- [ src[n-2][0] + src[n-2][1] | src[n-2][1] + src[n-2][2] ]
-
-; 		paddw        xmm5, xmm7 ; xmm5 <- [  src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1]    |  src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2]    ]
-; 		psrlw        xmm5, 2    ; xmm5 <- [ (src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1])/4 | (src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2])/4 ]
-
-; 		packuswb     xmm5, xmm8 ; xmm5 <- [ 0 | 0 | (src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1])/4 | (src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2])/4 ]
-
-; 		; --------------------------------------------------------------------------------------------------------------------------------------------------
-
-; 		; xmm0 <- [ src[n-1][0] | src[n-1][1] |                         src[n-1][2]                       |                         src[n-1][3]                       ]
-; 		; xmm1 <- [ src[n-2][0] | src[n-2][1] |                         src[n-2][2]                       |                         src[n-2][3]                       ]
-; 		; xmm2 <- [      0      |      0      |               (src[n-1][0] + src[n-2][0])/2               |               (src[n-1][1] + src[n-2][1])/2               ]
-; 		; xmm4 <- [      0      |      0      |               (src[n-1][0] + src[n-1][1])/2               |               (src[n-1][1] + src[n-1][2])/2               ]
-; 		; xmm6 <- [      0      |      0      |               (src[n-2][0] + src[n-2][1])/2               |               (src[n-2][1] + src[n-2][2])/2               ]
-; 		; xmm5 <- [      0      |      0      | (src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1])/4 | (src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2])/4 ]
-
-
-; 		movdqu       xmm9, xmm2 ; xmm9 == xmm2
-; 		psrldq       xmm2, 4    ; xmm2 <- [              0                |      0      |              0                | (src[n-1][0] + src[n-2][0])/2 ]
-; 		pslldq       xmm2, 12   ; xmm2 <- [ (src[n-1][0] + src[n-2][0])/2 |      0      |              0                |              0                ]
-; 		pslldq       xmm9, 12   ; xmm9 <- [ (src[n-1][1] + src[n-2][1])/2 |      0      |              0                |              0                ]
-; 		psrldq       xmm9, 8    ; xmm9 <- [              0                |      0      | (src[n-1][1] + src[n-2][1])/2 |              0                ]
-; 		por          xmm2, xmm9 ; xmm2 <- [ (src[n-1][0] + src[n-2][0])/2 |      0      | (src[n-1][1] + src[n-2][1])/2 |              0                ]
-
-
-; 		movdqu       xmm9, xmm5 ; xmm9 == xmm5
-; 		psrldq       xmm5, 4    ; xmm5 <- [                            0                              |                            0                              |      0      | (src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1])/4 ]
-; 		pslldq       xmm5, 8    ; xmm5 <- [                            0                              | (src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1])/4 |      0      |                            0                              ]
-; 		pslldq       xmm9, 12   ; xmm9 <- [ (src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2])/4 |                            0                              |      0      |                            0                              ]
-; 		psrldq       xmm9, 12   ; xmm9 <- [                            0                              |                            0                              |      0      | (src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2])/4 ]
-; 		por          xmm5, xmm9 ; xmm5 <- [                            0                              | (src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1])/4 |      0      | (src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2])/4 ]
-
-; 		por          xmm2, xmm5 ; xmm2 <- [ (src[n-1][0] + src[n-2][0])/2 | (src[n-1][0] + src[n-1][1] + src[n-2][0] + src[n-2][1])/4 | (src[n-1][1] + src[n-2][1])/2 | (src[n-1][1] + src[n-1][2] + src[n-2][1] + src[n-2][2])/4 ]
-
-
-
-
-; 		movdqu       xmm9, xmm4 ; xmm9 == xmm4
-; 		psrldq       xmm4, 4    ; xmm4 <- [              0                |              0                |      0      | (src[n-1][0] + src[n-1][1])/2 ]
-; 		pslldq       xmm4, 8    ; xmm4 <- [              0                | (src[n-1][0] + src[n-1][1])/2 |      0      |              0                ]
-; 		pslldq       xmm9, 12   ; xmm9 <- [ (src[n-1][1] + src[n-1][2])/2 |              0                |      0      |              0                ]
-; 		psrldq       xmm9, 12   ; xmm9 <- [              0                |              0                |      0      | (src[n-1][1] + src[n-1][2])/2 ]
-; 		por          xmm4, xmm9 ; xmm4 <- [              0                | (src[n-1][0] + src[n-1][1])/2 |      0      | (src[n-1][1] + src[n-1][2])/2 ]
-
-
-
-
-; 		movdqu       xmm9, xmm6 ; xmm9 == xmm6
-; 		psrldq       xmm6, 4    ; xmm6 <- [              0                |              0                |      0      | (src[n-2][0] + src[n-2][1])/2 ]
-; 		pslldq       xmm6, 8    ; xmm6 <- [              0                | (src[n-2][0] + src[n-2][1])/2 |      0      |              0                ]
-; 		pslldq       xmm9, 12   ; xmm9 <- [ (src[n-2][1] + src[n-2][2])/2 |              0                |      0      |              0                ]
-; 		psrldq       xmm9, 12   ; xmm9 <- [              0                |              0                |      0      | (src[n-2][1] + src[n-2][2])/2 ]
-; 		por          xmm6, xmm9 ; xmm6 <- [              0                | (src[n-2][0] + src[n-2][1])/2 |      0      | (src[n-2][1] + src[n-2][2])/2 ]
-
-
-
-
-
-
-; 		movdqu       xmm3, xmm0 ; xmm3 == xmm0
-; 		psrldq       xmm3, 8    ; xmm3 <- [      0      |      0      | src[n-1][0] | src[n-1][1] ]
-; 		movdqu       xmm9, xmm3 ; xmm9 == xmm3
-; 		psrldq       xmm3, 4    ; xmm3 <- [      0      |      0      |      0      | src[n-1][0] ]
-; 		pslldq       xmm3, 12   ; xmm3 <- [ src[n-1][0] |      0      |      0      |      0      ]
-; 		pslldq       xmm9, 12   ; xmm9 <- [ src[n-1][1] |      0      |      0      |      0      ]
-; 		psrldq       xmm9, 8    ; xmm9 <- [      0      |      0      | src[n-1][1] |      0      ]
-; 		por          xmm3, xmm9 ; xmm3 <- [ src[n-1][0] |      0      | src[n-1][1] |      0      ]
-
-; 		por          xmm3, xmm4 ; xmm3 <- [ src[n-1][0] | (src[n-1][0] + src[n-1][1])/2 | src[n-1][1] | (src[n-1][1] + src[n-1][2])/2 ]
-
-
-; 		movdqu       xmm5, xmm1 ; xmm5 == xmm1
-; 		psrldq       xmm5, 8    ; xmm5 <- [      0      |      0      | src[n-2][0] | src[n-2][1] ]
-; 		movdqu       xmm9, xmm5 ; xmm9 == xmm5
-; 		psrldq       xmm5, 4    ; xmm5 <- [      0      |      0      |      0      | src[n-2][0] ]
-; 		pslldq       xmm5, 12   ; xmm5 <- [ src[n-2][0] |      0      |      0      |      0      ]
-; 		pslldq       xmm9, 12   ; xmm9 <- [ src[n-2][1] |      0      |      0      |      0      ]
-; 		psrldq       xmm9, 8    ; xmm9 <- [      0      |      0      | src[n-2][1] |      0      ]
-; 		por          xmm5, xmm9 ; xmm5 <- [ src[n-2][0] |      0      | src[n-2][1] |      0      ]
-
-; 		por          xmm5, xmm6 ; xmm5 <- [ src[n-2][0] | (src[n-2][0] + src[n-2][1])/2 | src[n-2][1] | (src[n-2][1] + src[n-2][2])/2 ]
